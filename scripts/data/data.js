33d0b25ebc0975151d33ffc1f76ff9bb0c4276de
pvianap <pedro.v.parente@gmail.com>
2022-08-21 17:42:54 +0200

    End of loop version and docs

--- scripts/data/data.js
+++ scripts/data/data.js
@@ -14,29 +14,51 @@ class Data {
 
   getIngredients(data) {
     const ingredients = [];
-    data.forEach((element) => {
-      element.ingredients.forEach((e) => {
-        ingredients.push(e.ingredient);
-      });
-    });
+
+    // NATIVE CODE
+
+    // data.forEach((element) => {
+    //   element.ingredients.forEach((e) => {
+    //     ingredients.push(e.ingredient);
+    //   });
+    // });
+
+    // LOOP CODE
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].ingredients.length; j++) {
+        ingredients.push(data[i].ingredients[j].ingredient);
+      }
+    }
     return [...new Set(ingredients)].sort();
   }
 
   getAppliance(data) {
     const appliances = [];
-    data.forEach((element) => {
-      appliances.push(element.appliance);
-    });
-    return [...new Set(appliances)].sort();
+    // data.forEach((element) => {
+    //   appliances.push(element.appliance);
+    // });
+
+    for (let i = 0; i < data.length; i++) {
+      appliances.push(data[i].appliance);
+    }
+
+    // return [...new Set(appliances)].sort();
+    return [...new Set(appliances)];
   }
 
   getUstensiles(data) {
     const ustensils = [];
-    data.forEach((element) => {
-      element.ustensils.forEach((e) => {
-        ustensils.push(e);
-      });
-    });
+    // data.forEach((element) => {
+    //   element.ustensils.forEach((e) => {
+    //     ustensils.push(e);
+    //   });
+    // });
+
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].ustensils.length; j++) {
+        ustensils.push(data[i].ustensils[j]);
+      }
+    }
 
     return [...new Set(ustensils)].sort();
   }
